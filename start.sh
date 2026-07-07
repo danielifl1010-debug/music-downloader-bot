4ab38f5d3153ddfc849f84de6b9c1cb4d38aacfd
danielifl1010-debug <danielifl1010@gmail.com>
2026-07-08 01:14:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+export DENO_INSTALL="/opt/render/project/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+
+echo "DENO:"
+deno --version
+
+spotdl --download-deno
+
 gunicorn app:app --timeout 300
